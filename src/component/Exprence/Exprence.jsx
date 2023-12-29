4e6fcd32900002e0ea14cdc043360f291d1654fb
hridayroy19 <hrhridoyroy503@gmail.com>
2023-12-30 00:46:45 +0600

    renposive

--- src/component/Exprence/Exprence.jsx
+++ src/component/Exprence/Exprence.jsx
@@ -5,12 +5,12 @@ const Exprence = () => {
             
            <h1 className="text-center  items-center text-2xl font-bold text-green-500 mt-4 mb-5 "> My Expence</h1>
 
-<div className=" grid lg:grid-cols-3 mx-auto w-full grid-cols-1 ">
+<div className=" grid lg:grid-cols-3 gap-4 mx-auto w-full grid-cols-1 ">
 
   <div data-aos="fade-right">
 
  
-<div className="card w-[340px]  bg-base-300 border-[5px]">
+<div className="card lg:w-[340px] w-[250px]  bg-base-300 border-[5px]">
   <figure><img src="https://i.ibb.co/dp0Drgs/147792698-web-development-icon-simple-illustration-from-creative-package-collection-creative-web-dev.jpg" alt="Shoes" className="p-6 w-[50%]  " /></figure>
   <div className="card-body">
     <h2 className="card-title">
@@ -27,7 +27,7 @@ const Exprence = () => {
 </div>
 </div>
 <div data-aos="fade-down">
-<div className="card  w-[340px] bg-base-300 border-[5px]">
+<div className="card  lg:w-[340px] w-[250px]  bg-base-300 border-[5px]">
   <figure><img src="https://i.ibb.co/dp0Drgs/147792698-web-development-icon-simple-illustration-from-creative-package-collection-creative-web-dev.jpg" alt="Shoes" className="p-6 w-[50%]  " /></figure>
   <div className="card-body">
     <h2 className="card-title">
@@ -44,7 +44,7 @@ const Exprence = () => {
 </div>
 </div>
 <div data-aos="fade-left">
-<div className="card  w-[340px]  bg-base-300 border-[5px]">
+<div className="card  lg:w-[340px] w-[250px]   bg-base-300 border-[5px]">
   <figure><img src="https://i.ibb.co/dp0Drgs/147792698-web-development-icon-simple-illustration-from-creative-package-collection-creative-web-dev.jpg" alt="Shoes" className="p-6 w-[50%]  " /></figure>
   <div className="card-body">
     <h2 className="card-title">
--- src/component/Skaill/Skaill.jsx
+++ src/component/Skaill/Skaill.jsx
@@ -2,11 +2,11 @@
 const Skaill = () => {
     return (
         <div className=" py-8">
-          <h2 className="text-3xl font-bold  mb-9 uppercase mt-6 text-green-500"> My Skell</h2>
+          <h2 className="text-3xl font-bold mt-40  mb-9 uppercase lg:mt-6 text-green-500"> My Skell</h2>
           <div className="grid lg:grid-cols-4 gap-4 md:grid-cols-2 grid-cols-1">
             {/* fast */}
 <div  data-aos="fade-right">
-<div className=" w-[300px] px-4 rounded-md bg-gray-300 items-center text-center h-[100px] gap-3 flex shadow-xl">
+<div className=" lg:w-[300px] w-[250px] px-4 rounded-md bg-gray-300 items-center text-center h-[100px]  gap-3 flex shadow-xl">
 <img src="https://i.ibb.co/jDvF0xz/mongodb-logo-D13-D67-C930-seeklogo-com.png" alt="Movie" className="w-[20%] items-center  "/>
   <div className="">
     <h2 className="">New movie is released!
@@ -15,7 +15,7 @@ const Skaill = () => {
 </div>
 </div>
             {/* fast */}
-          <div className=" w-[300px] px-4  rounded-md bg-gray-300 items-center text-center h-[100px] gap-3 flex shadow-xl">
+          <div className="lg:w-[300px] w-[250px] px-4  rounded-md bg-gray-300 items-center text-center h-[100px] gap-3 flex shadow-xl">
 <img src="https://i.ibb.co/hBkXWr2/download.jpg" alt="Movie" className="w-[20%] items-center rounded-[10px]   "/>
   <div className="">
     <h2 className="">New movie is released!
@@ -23,7 +23,7 @@ const Skaill = () => {
   </div>
 </div>
             {/* 2fast */}
-          <div className=" w-[300px] px-4 rounded-md bg-gray-300 items-center text-center h-[100px] gap-3 flex shadow-xl">
+          <div className=" lg:w-[300px] w-[250px] px-4 rounded-md bg-gray-300 items-center text-center h-[100px] gap-3 flex shadow-xl">
 <img src="https://i.ibb.co/fD2C9HJ/download-1.jpg" alt="Movie" className="w-[20%] items-center rounded-[10px]  "/>
   <div className="">
     <h2 className="">New movie is released!
@@ -32,7 +32,7 @@ const Skaill = () => {
 </div>
             {/* fastc    data-aos="fade-left" */}
 <div data-aos="fade-left">
-<div className=" w-[300px] px-4  rounded-md bg-gray-300 items-center text-center h-[100px] gap-3 flex shadow-xl">
+<div className=" lg:w-[300px] w-[250px] px-4  rounded-md bg-gray-300 items-center text-center h-[100px] gap-3 flex shadow-xl">
 <img src="https://i.ibb.co/YRfjsJs/282599.webp" alt="Movie" className="w-[20%] items-center rounded-[10px]   "/>
   <div className="">
     <h2 className="">New movie is released!
